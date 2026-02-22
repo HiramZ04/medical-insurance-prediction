028e2120abb4ef933f58e188cb77eb43c81e4144
Andres Gonzalez <nicoseptiencool@gmail.com>
2026-02-21 19:57:18 -0800

    Add initial web app code

--- app/app.js
+++ app/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile('index.html', { root: __dirname + '/public' });
+});
+
+app.get('/main.js', (req, res) => {
+  res.sendFile('main.js', { root: __dirname + '/src' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
